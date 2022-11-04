e13fdfb957589da23a3e8519277b9cf8dc0d1aa4
beckerw <william.becker.externe@ifpen.fr>
2022-11-04 11:43:50 +0100

    Angular components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
     declarations: [
@@ -20,7 +23,10 @@ import {MatListModule} from "@angular/material/list";
         BrowserAnimationsModule,
         FlexLayoutModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
