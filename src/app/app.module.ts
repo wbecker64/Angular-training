ba35db8e4730758132f695204f4ee8b099de00c6
beckerw <william.becker.externe@ifpen.fr>
2022-11-04 10:12:56 +0100

    Components part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from "@angular/flex-layout";
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MenuComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FlexLayoutModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,18 @@
+export class Dish {
+    // @ts-ignore
+    id: string;
+    // @ts-ignore
+    name: string;
+    // @ts-ignore
+    image: string;
+    // @ts-ignore
+    category: string;
+    // @ts-ignore
+    featured: boolean;
+    // @ts-ignore
+    label: string;
+    // @ts-ignore
+    price: string;
+    // @ts-ignore
+    description: string;
+}
