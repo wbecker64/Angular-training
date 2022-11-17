56229c9dd387624a6a10c0ed6964b89080b80db4
beckerw <william.becker.externe@ifpen.fr>
2022-11-17 16:58:58 +0100

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,9 @@ import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatCheckboxModule} from "@angular/material/checkbox";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @NgModule({
     declarations: [
@@ -57,7 +59,10 @@ import {FormsModule} from "@angular/forms";
         MatFormFieldModule,
         MatInputModule,
         MatCheckboxModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatSlideToggleModule
     ],
     entryComponents: [
         LoginComponent
