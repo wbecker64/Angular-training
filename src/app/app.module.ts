82f2c89aa64cf3b4d1b405ea7dba12ad0b01b627
beckerw <william.becker.externe@ifpen.fr>
2022-11-17 15:44:40 +0100

    Template Driven Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,10 @@ import {PromotionService} from "./services/promotion.service";
 import {LeaderService} from "./services/leader.service";
 import { LoginComponent } from './login/login.component';
 import {MatDialogModule} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -49,7 +53,11 @@ import {MatDialogModule} from "@angular/material/dialog";
         MatCardModule,
         MatButtonModule,
         MatExpansionModule,
-        MatDialogModule
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        FormsModule
     ],
     entryComponents: [
         LoginComponent
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = {username: '', password: '', remember: false}
+
+  constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log("User: ", this.user)
+    this.dialogRef.close()
   }
 
 }
